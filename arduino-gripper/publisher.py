c0459ee2ac231672a48627ba562eb974b6084d6f
Adam Lim <79502321+Jung028@users.noreply.github.com>
2024-05-30 12:49:14 +0800

    arduino-ros

---
+++ arduino-gripper/publisher.py
@@ -0,0 +1,25 @@
+
+
+import rospy
+from custom_msgs.msg import GripperControl
+
+def gripper_command_publisher():
+    rospy.init_node('gripper_command_publisher', anonymous=True)
+    gripper_control_pub = rospy.Publisher('/gripper_control', GripperControl, queue_size=10)
+
+    rate = rospy.Rate(10)  
+
+    while not rospy.is_shutdown():
+        command = input("Enter gripper command (o/c): ")
+
+        gripper_control_msg = GripperControl()
+        gripper_control_msg.command = command
+        gripper_control_pub.publish(gripper_control_msg)
+
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        gripper_command_publisher()
+    except rospy.ROSInterruptException:
+        pass
---
+++ arduino-gripper/ros-arduino.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import rospy
+from custom_msgs.msg import GripperControl 
+import serial
+
+ARDUINO_PORT = '/dev/ttyS0' #port on wei wen's computer for arduino 
+BAUD_RATE = 9600 
+
+arduino_serial = serial.Serial(ARDUINO_PORT, BAUD_RATE, timeout=1)
+
+def gripper_control_callback(msg):
+    command = msg.command
+    
+    arduino_serial.write(command.encode()) #writes the command to the arduino port, sending the command to it to o (open), or c(close)
+
+def gripper_control_node():
+    rospy.init_node('gripper_control_node', anonymous=True)
+    rospy.Subscriber('/gripper_control', GripperControl, gripper_control_callback)
+    rospy.spin()
+
+def getDepth(): 
+    #'/camera/depth/image_raw'
+    # get the values from this topic, and based on the conditions, if met, send o/c command to the gripper_control topic
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        gripper_control_node()
+    except rospy.ROSInterruptException:
+        pass
