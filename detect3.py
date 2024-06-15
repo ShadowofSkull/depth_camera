f780857cc1106dbb5bf1c83c3e1bc5cbd57c7001
John Doe <johndoe@example.com>
2024-06-15 17:11:10 +0900

    publish to motor control, obtain real x

--- detect3.py
+++ detect3.py
@@ -4,7 +4,7 @@ import time
 import torch
 import rospy
 from sensor_msgs.msg import Image
-from astra_camera.msg import CoordsMatrix, Coords, XZ, XZs
+from astra_camera.msg import CoordsMatrix, Coords, XZ, XZs, MotorControl, GripperControl
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 from ultralytics import YOLO
@@ -22,8 +22,8 @@ def callback(colorFrame, depthFrame):
     frame_counter += 1
 
     # Process every 5th frame
-    # if frame_counter % 60 != 0:
-    if frame_counter % 5 != 0:
+    if frame_counter % 60 != 0:
+    # if frame_counter % 5 != 0:
         return
 
     # Convert ROS msg to cv nparray that's suitable for model
@@ -48,6 +48,7 @@ def callback(colorFrame, depthFrame):
     for result in results:
         # Obtain classes name model can detect
         names = result.names
+        print(f"name: {names}")
         boxes = result.boxes
         for box in boxes:
             xywh = box.xywh
@@ -65,13 +66,13 @@ def callback(colorFrame, depthFrame):
             x = int(x)
             y = int(y)
 
-            depth = getDepth(x, y, conf, clsName, colorFrame)
+            depth = getDepth(x, y, conf, clsName, depthFrame)
             real_x = calcX(depth, x, colorFrame)
             # Publish xz to a topic so node can use it
-            xz = XZ()
-            xz.x = real_x
-            xz.z = depth
-            realXZs.append(xz)
+            # xz = XZ()
+            # xz.x = real_x
+            # xz.z = depth
+            realXZs.append((real_x, depth))
             print(f"realx:{real_x}, depth:{depth}")
             # If need publish only do this
             # coord = Coords()
@@ -81,10 +82,12 @@ def callback(colorFrame, depthFrame):
             # coord.cls = clsName
             # depthCoords.append(coord)
 
+            
+
             # Visualize the results on the frame (optional)
             annotated_frame = result.plot()
             # Draw a red dot at the centre of the box illustration purpose
-            annotated_frame[y : y + 5, x : x + 5] = [0, 0, 255]
+            # annotated_frame[y : y + 5, x : x + 5] = [0, 0, 255]
 
         # Display the annotated frame (optional)
         # try:
@@ -93,35 +96,51 @@ def callback(colorFrame, depthFrame):
         # except:
         #     print("failed")
 
+    # list contains x, z of ball
+    closestBallXZ = [0, 0]
+    for i in range(len(realXZs)):
+        if realXZs[i][1] < closestBallXZ[1]:
+            closestBallXZ[0] = realXZs[i][0]
+            closestBallXZ[1] = realXZs[i][1]
+    
+    # gripperMsg = GripperControl()
+    motorMsg = MotorControl()
+    motorMsg.horizontal = closestBallXZ[0]
+    motorMsg.forward = closestBallXZ[1]
+    print(motorMsg)
+    pubMotorControl.publish(motorMsg)
+    # if 
+    # gripperMsg.grip = "o"
+    # gripperMsg.flip = 
+
     # Publish xz to a topic for node to use
-    msg.xzs = realXZs
-    print(msg.xzs)
-    pubXZs.publish(msg)
+    # msg.xzs = realXZs
+    # print(msg.xzs)
+    # pubXZs.publish(msg)
+
 
 
 def calcX(depth, x, colorFrame):
-    fov_H = 60
     resolution_w = colorFrame.shape[1]
-    center_x = resolution_w / 2
-    if x <= center_x:
-        theta_x = fov_H / resolution_w * (center_x - x)
-    theta_x = fov_H / resolution_w * (x - center_x)
-    real_x = math.tan(theta_x) * depth
-    print(f"real x: {real_x}mm")
+    center_x = resolution_w // 2
+    # need to obtain using f = (xpixel - center x) * distance / real x
+    focal = 580
+    real_x = (x - center_x) * (depth / focal)
     return real_x
 
 
-def getDepth(x, y, conf, clsName, colorFrame):
-    padded_img = cv2.copyMakeBorder(
-        colorFrame, pad_size, pad_size, pad_size, pad_size, cv2.BORDER_CONSTANT, value=0
-    )
+
+def getDepth(x, y, conf, clsName, depthFrame):
     # Create a max spatial filter to get the max value in a 3x3 or bigger window
-    window_size = 21
+    window_size = 5
     # window_size change pattern = 3 + 2x, x = 0, 1, 2,...
     # pad_size change pattern = 1 + x, x = 0, 1, 2,...
     # Do some substitution to get the formula
     # pad = (win - 1) // 2
     pad_size = (window_size - 1) // 2
+    padded_img = cv2.copyMakeBorder(
+        depthFrame, pad_size, pad_size, pad_size, pad_size, cv2.BORDER_CONSTANT, value=0
+    )
     pix_in_win = padded_img[
         y - pad_size : y + pad_size + 1, x - pad_size : x + pad_size + 1
     ]
@@ -137,10 +156,13 @@ if __name__ == "__main__":
     rospy.init_node("detect")
     start = time.time()
     model = YOLO("./models/best.pt")
-    msg = XZs()
+    # msg = XZs()
+
     print("done init")
     # pubCoords = rospy.Publisher("coords", CoordsMatrix, queue_size=10)
-    pubXZs = rospy.Publisher("xz", XZs, queue_size=10)
+    # pubXZs = rospy.Publisher("xz", XZs, queue_size=10)
+    pubGripperControl = rospy.Publisher("gripperControl", GripperControl, queue_size=10)
+    pubMotorControl = rospy.Publisher("motorControl", MotorControl, queue_size=10)
     colorSub = message_filters.Subscriber("/camera/color/image_raw", Image)
     depthSub = message_filters.Subscriber("/camera/depth/image_raw", Image)
     ts = message_filters.ApproximateTimeSynchronizer(
