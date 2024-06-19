089b3c5195b957988f4234a79251758a9c67221d
John Doe <johndoe@example.com>
2024-06-19 21:50:18 +0900

    added gripper flip state

--- MotorControl/MotorControl.ino
+++ MotorControl/MotorControl.ino
@@ -1,15 +1,14 @@
-#include <ros.h>
-#include <std_msgs/Float32.h>
-#include <astra_camera/MotorControl.h>  // Replace 'astra_camera' with your actual ROS package name
-
 #include <util/atomic.h> // For the ATOMIC_BLOCK macro
 #include "CytronMotorDriver.h"
 #include <math.h>
+#include <ros.h>
+#include <std_msgs/Int8.h>
+#include <std_msgs/String.h>
 
 #define ENCA1 18 // Encoder 1 (LB)
 #define ENCB1 17 // Encoder 1
-#define ENCA2 2  // Encoder 2 (LF)
-#define ENCB2 3  // Encoder 2
+#define ENCA2 2 // Encoder 2 (LF)
+#define ENCB2 3 // Encoder 2
 #define ENCA3 19 // Encoder 3 (RF)
 #define ENCB3 20 // Encoder 3
 #define ENCA4 21 // Encoder 4 (RB)
@@ -18,151 +17,177 @@
 volatile int posi1 = 0, posi2 = 0, posi3 = 0, posi4 = 0; // Positions
 int pos1, pos2, pos3, pos4;
 long prevT = 0;
-float eprev1 = 0, eprev2 = 0, eprev3 = 0, eprev4 = 0;
-float eintegral1 = 0, eintegral2 = 0, eintegral3 = 0, eintegral4 = 0;
-float u1 = 0, u2 = 0, u3 = 0, u4 = 0;
-float target1, target2, target3, target4;
+float eprev = 0;
+float eintegral = 0;
+float u = 0;
+float target = 0;
 
-CytronMD motor1(PWM_DIR, 4, 5); // Motor 1 (Left Back)
-CytronMD motor2(PWM_DIR, 6, 7); // Motor 2 (Left Front)
-CytronMD motor3(PWM_DIR, 8, 9); // Motor 3 (Right Front)
-CytronMD motor4(PWM_DIR, 10, 11); // Motor 4 (Right Back)
+CytronMD motor1(PWM_DIR, 4, 5); // Motor 1 (LB)
+CytronMD motor2(PWM_DIR, 6, 7); // Motor 2 (LF)
+CytronMD motor3(PWM_DIR, 8, 9); // Motor 3 (RF)
+CytronMD motor4(PWM_DIR, 10, 11); // Motor 4 (RB)
 
+// Define states
+enum MotorState {
+    RUNNING,
+    STOPPED
+};
+MotorState motorState = STOPPED;
+
+// ROS NodeHandle
 ros::NodeHandle nh;
 
-// Define callback function to receive motor control messages
-void motorControlCallback(const astra_camera::MotorControl& msg) {
-  int16_t x = msg.x;  // Horizontal values
-  int16_t z = msg.z;  // Depth values
+// Callback functions for ROS subscribers
+void motorControlCallback(const astra_camera::MotorControl &msg) {
+    int16_t x = msg.x;
+    int16_t z = msg.z;
 
-  float speed = 255.0; // Assuming max speed of 255
-  float timeToRun = abs(z) / speed; // Time to run the motor based on distance and speed
-
-  if (z > 0) {
-    // Move forward
-    motor1.setSpeed(speed);
-    motor2.setSpeed(speed);
-    motor3.setSpeed(speed);
-    motor4.setSpeed(speed);
-  } else if (z < 0) {
-    // Move backward
-    motor1.setSpeed(-speed);
-    motor2.setSpeed(-speed);
-    motor3.setSpeed(-speed);
-    motor4.setSpeed(-speed);
-  } else if (x > 0) {
-    // Move right
-    motor1.setSpeed(-speed);
-    motor2.setSpeed(speed);
-    motor3.setSpeed(speed);
-    motor4.setSpeed(-speed);
-  } else if (x < 0) {
-    // Move left
-    motor1.setSpeed(speed);
-    motor2.setSpeed(-speed);
-    motor3.setSpeed(-speed);
-    motor4.setSpeed(speed);
-  } else if (x > 0 && z > 0) {
-    // Move diagonally top-right
-    motor1.setSpeed(0);
-    motor2.setSpeed(speed);
-    motor3.setSpeed(speed);
-    motor4.setSpeed(0);
-  } else if (x < 0 && z > 0) {
-    // Move diagonally top-left
-    motor1.setSpeed(speed);
-    motor2.setSpeed(0);
-    motor3.setSpeed(0);
-    motor4.setSpeed(speed);
-  } else if (x > 0 && z < 0) {
-    // Move diagonally bottom-right
-    motor1.setSpeed(-speed);
-    motor2.setSpeed(0);
-    motor3.setSpeed(0);
-    motor4.setSpeed(-speed);
-  } else if (x < 0 && z < 0) {
-    // Move diagonally bottom-left
-    motor1.setSpeed(0);
-    motor2.setSpeed(-speed);
-    motor3.setSpeed(-speed);
-    motor4.setSpeed(0);
-  }
-
-  delay(timeToRun * 1000); // Convert to milliseconds
-
-  // Stop the motors after moving
-  motor1.setSpeed(0);
-  motor2.setSpeed(0);
-  motor3.setSpeed(0);
-  motor4.setSpeed(0);
+    // Handle the x and z values as needed
+    // For this example, we just print them
+    Serial.print("x: ");
+    Serial.print(x);
+    Serial.print(", z: ");
+    Serial.println(z);
 }
 
-ros::Subscriber<astra_camera::MotorControl> sub("MotorControl", motorControlCallback);
+void gripperControlCallback(const astra_camera::GripperControl &msg) {
+    if (msg.flip == "forward") {
+        // Set motors to move forward
+        motorState = RUNNING;
+        target = 1000 ; // Set appropriate target for moving forward
+    } else if (msg.flip == "backward") {
+        // Set motors to move backward
+        motorState = RUNNING;
+        target = -1000 ; // Set appropriate target for moving backward
+    }
+}
+
+// ROS Subscribers
+ros::Subscriber<astra_camera::MotorControl> motorControlSub("motor_control", motorControlCallback);
+ros::Subscriber<astra_camera::GripperControl> gripperControlSub("gripper_control", gripperControlCallback);
 
 void setup() {
-  Serial.begin(115200);
-  nh.initNode();
-  nh.subscribe(sub);
+    Serial.begin(115200);
 
-  // Set encoder pins as inputs with pull-up resistors
-  pinMode(ENCA1, INPUT_PULLUP);
-  pinMode(ENCB1, INPUT_PULLUP);
-  pinMode(ENCA2, INPUT_PULLUP);
-  pinMode(ENCB2, INPUT_PULLUP);
-  pinMode(ENCA3, INPUT_PULLUP);
-  pinMode(ENCB3, INPUT_PULLUP);
-  pinMode(ENCA4, INPUT_PULLUP);
-  pinMode(ENCB4, INPUT_PULLUP);
+    // Set encoder pins as inputs with pull-up resistors
+    pinMode(ENCA1, INPUT_PULLUP);
+    pinMode(ENCB1, INPUT_PULLUP);
+    pinMode(ENCA2, INPUT_PULLUP);
+    pinMode(ENCB2, INPUT_PULLUP);
+    pinMode(ENCA3, INPUT_PULLUP);
+    pinMode(ENCB3, INPUT_PULLUP);
+    pinMode(ENCA4, INPUT_PULLUP);
+    pinMode(ENCB4, INPUT_PULLUP);
+    
+    attachInterrupt(digitalPinToInterrupt(ENCA1), readEncoder1, RISING);
+    attachInterrupt(digitalPinToInterrupt(ENCA2), readEncoder2, RISING);
+    attachInterrupt(digitalPinToInterrupt(ENCA3), readEncoder3, RISING);
+    attachInterrupt(digitalPinToInterrupt(ENCA4), readEncoder4, RISING);
 
-  attachInterrupt(digitalPinToInterrupt(ENCA1), readEncoder1, RISING);
-  attachInterrupt(digitalPinToInterrupt(ENCA2), readEncoder2, RISING);
-  attachInterrupt(digitalPinToInterrupt(ENCA3), readEncoder3, RISING);
-  attachInterrupt(digitalPinToInterrupt(ENCA4), readEncoder4, RISING);
+    motor1.setSpeed(0);
+    motor2.setSpeed(0);
+    motor3.setSpeed(0);
+    motor4.setSpeed(0);
+    delay(10);
 
-  motor1.setSpeed(0);
-  motor2.setSpeed(0);
-  motor3.setSpeed(0);
-  motor4.setSpeed(0);
-  delay(10);
-  pos1 = 0;
-  pos2 = 0;
-  pos3 = 0;
-  pos4 = 0;
+    pos1 = 0;
+    pos2 = 0;
+    pos3 = 0;
+    pos4 = 0;
+
+    // Initialize ROS node handle and subscribers
+    nh.initNode();
+    nh.subscribe(motorControlSub);
+    nh.subscribe(gripperControlSub);
 }
 
 void loop() {
-  nh.spinOnce();
-  delay(10); // Small delay to prevent overwhelming the CPU
-}
+    // Read serial input and update target
+    if (Serial.available()) {
+        String command = Serial.readStringUntil('\n');
+        target = command.toFloat() * 6 / PI; // Update target position
+        Serial.println(target);
 
-void readEncoder1() {
-  if (digitalRead(ENCB1) == HIGH) {
-    posi1++;
-  } else {
-    posi1--;
-  }
-}
+        // Reset motor state to RUNNING when new target is set
+        motorState = RUNNING;
+    }
 
-void readEncoder2() {
-  if (digitalRead(ENCB2) == HIGH) {
-    posi2++;
-  } else {
-    posi2--;
-  }
-}
+    // PID constants - motor 1 and motor 2 and motor 4
+    float kp = 10.0;
+    float kd = 1.0;
+    float ki = 0.1;
 
-void readEncoder3() {
-  if (digitalRead(ENCB3) == HIGH) {
-    posi3++;
-  } else {
-    posi3--;
-  }
-}
+    // time difference
+    long currT = micros();
+    float deltaT = ((float)(currT - prevT)) / (1.0e6);
+    prevT = currT;
 
-void readEncoder4() {
-  if (digitalRead(ENCB4) == HIGH) {
-    posi4++;
-  } else {
-    posi4--;
-  }
-}
+    // Read the position in an atomic block to avoid a potential
+    // misread if the interrupt coincides with this code running
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+        pos1 = posi1;
+        pos2 = posi2;
+        pos3 = posi3;
+        pos4 = posi4;
+    }
+
+    int pos = (pos1 + pos2 + pos3 + pos4) / 4;
+
+    // error
+    int e = target - pos; // motor 1 and motor 3 and motor 4
+
+    // derivative
+    float dedt = (e - eprev) / deltaT;
+
+    // integral
+    eintegral += e * deltaT;
+
+    // control signal
+    float u = kp * e;
+
+    // Limit control signal to prevent excessive motor speed
+    if (u > 75) u = 75;
+    else if (u < -75) u = -75;
+
+    // Set motor speeds based on current state
+    if (motorState == RUNNING) {
+        motor1.setSpeed(u); // Motor 1 runs forward
+        motor2.setSpeed(u); // Motor 2 and Motor 3 and Motor 4 runs forward
+        motor3.setSpeed(u); // Motor 2 and Motor 3 and Motor 4 runs forward
+        motor4.setSpeed(u); // Motor 2 and Motor 3 and Motor 4 runs forward
+    } else {
+        motor1.setSpeed(0); // Stop Motor 1
+        motor2.setSpeed(0); // Stop Motor 2
+        motor3.setSpeed(0); // Stop Motor 3
+        motor4.setSpeed(0); // Stop Motor 4
+    }
+
+    // Store previous error
+    eprev = e;
+
+    // Print debug information
+    Serial.print(target);
+    Serial.print(" ");
+    Serial.print(pos);
+    Serial.print(" ");
+    Serial.print(pos1);
+    Serial.print(" ");
+    Serial.print(pos2);
+    Serial.print(" ");
+    Serial.print(pos3);
+    Serial.print(" ");
+    Serial.print(pos4);
+    Serial.print(" ");
+    Serial.println(u);
+
+    // Check if any motor has reached the target
+    if (motorState == RUNNING) {
+        if (pos1 == target || pos2 == target || pos3 == target || pos4 == target) {
+            // Stop all motors
+            motor1.setSpeed(0);
+            motor2.setSpeed(0);
+            motor3.setSpeed(0);
+            motor4.setSpeed(0);
+
+            // Set state to STOPPED
+            motorState = STOP
