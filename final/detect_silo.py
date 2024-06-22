85e6ba0a270c193a3b17fe9b1051d71bd416b7f6
John Doe <johndoe@example.com>
2024-06-22 09:30:57 +0800

    final commit done

--- final/detect_silo.py
+++ final/detect_silo.py
@@ -112,6 +112,7 @@ def callback(colorFrame, depthFrame):
             closestTeamBallXZ = findClosestBall(teamBallRealXZs)
             # print(f"TEAMclosest ball: {closestTeamBallXZ}")
             closestPurpleBallXZ = findClosestBall(purpleBallRealXZs)
+ 
             # print(f"purple closest ball {closestPurpleBallXZ}")
             if closestTeamBallXZ is None:
                 print("No closest team ball found, robot stop")
@@ -325,8 +326,7 @@ def ballPublishControl(closestTeamBallXZ, closestPurpleBallXZ):
     # Close when ball enter gripper range and flip backward no matter what color ( should be handle at ir)
 
     closestBall = min(teamBallZ, purpleBallZ)
-    # publish arm state no matter what
-    gripperMsg.flip = gripperArmState
+    
     # if team ball was closer, keep gripping team ball, or else release purple ball
     if closestBall == purpleBallZ and closestBall != teamBallZ:
         gripperClawState = "o"
