69f151137e6e019603eb2877a5302eeb212e5e86
ShadowofSkull <zhplana@gmail.com>
2024-07-15 15:54:15 +0800

    old working gripper test

--- final/test_serial/test_serial.ino
+++ final/test_serial/test_serial.ino
@@ -25,40 +25,12 @@ void setup() {
 }
 
 void loop() {
-  if (Serial.available() > 0 && motorState == STOPPED) {
-//    Serial.print(Serial.available());
-    String command = Serial.readStringUntil('\n');
-    if (command.startsWith("F")) {
-      // Move forward
-      target = command.substring(1).toFloat(); // Extract distance from command
-      dir = 'F';
-      // Serial.println("Front");
-    } else if (command.startsWith("B")) {
-      // Move backward
-      target = command.substring(1).toFloat(); // Extract distance from command
-      dir = 'B';
-            // Serial.println("Back");
-    } else if (command.startsWith("L")) {
-      // Move left
-      target = command.substring(1).toFloat(); // Extract distance from command
-      dir = 'L';
-            // Serial.println("Left");
-    } else if (command.startsWith("R")) {
-      // Move right
-      target = command.substring(1).toFloat(); // Extract distance from command
-      dir = 'R';
-            // Serial.println("Right");
-      
-    }
-  
-  }
+  if (Serial.available() > 0) {
+    Serial.print(Serial.available());
+    String data = Serial.readStringUntil('\n'); // Read data until newline
     Serial.print("Received: ");
-    Serial.print(dir); // Echo received data back
-    Serial.println(target);
-    
-    if (dir == 'B' && target == 200) {
-      motorState = STARTED;
-      Serial.println(motorState);
+    Serial.println(data); // Echo received data back
+    if (data == "forward") {
       for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
         // in steps of 1 degree
         myservo.write(pos);              // tell servo to go to position in variable 'pos'
@@ -68,11 +40,59 @@ void loop() {
         myservo.write(pos);              // tell servo to go to position in variable 'pos'
         delay(15);                       // waits 15 ms for the servo to reach the position
       }
-      motorState = STOPPED;
-      Serial.println(motorState);
     }
     Serial.read(); // To clear the serial buffer
 
-  
+  }
   delay(10);
+//   if (Serial.available() > 0 && motorState == STOPPED) {
+// //    Serial.print(Serial.available());
+//     String command = Serial.readStringUntil('\n');
+//     if (command.startsWith("F")) {
+//       // Move forward
+//       target = command.substring(1).toFloat(); // Extract distance from command
+//       dir = 'F';
+//       // Serial.println("Front");
+//     } else if (command.startsWith("B")) {
+//       // Move backward
+//       target = command.substring(1).toFloat(); // Extract distance from command
+//       dir = 'B';
+//             // Serial.println("Back");
+//     } else if (command.startsWith("L")) {
+//       // Move left
+//       target = command.substring(1).toFloat(); // Extract distance from command
+//       dir = 'L';
+//             // Serial.println("Left");
+//     } else if (command.startsWith("R")) {
+//       // Move right
+//       target = command.substring(1).toFloat(); // Extract distance from command
+//       dir = 'R';
+//             // Serial.println("Right");
+      
+//     }
+  
+//   }
+//     Serial.print("Received: ");
+//     Serial.print(dir); // Echo received data back
+//     Serial.println(target);
+    
+//     if (dir == 'B' && target == 200) {
+//       motorState = STARTED;
+//       Serial.println(motorState);
+//       for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+//         // in steps of 1 degree
+//         myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//         delay(15);                       // waits 15 ms for the servo to reach the position
+//       }
+//       for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+//         myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//         delay(15);                       // waits 15 ms for the servo to reach the position
+//       }
+//       motorState = STOPPED;
+//       Serial.println(motorState);
+//     }
+//     Serial.read(); // To clear the serial buffer
+
+  
+//   delay(10);
 }
